6932b01e063c818f9a2ee61603ff8c3a4bd390cd
byteboss <adityasahrawat985@gmail.com>
2026-07-07 10:22:06 +0530

    chore: initial setup

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from 'express'
+
+const app = express();
+
+app.get("/health", (req, res) => {
+    return res.status(200).json({ message: "Server is running !" })
+})
+
+app.listen(3030, () => {
+    console.log("Server is listening on port 3030")
+})
