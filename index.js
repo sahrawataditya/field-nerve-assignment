bfe7aecea598572993dd98355033b2f4617e14b7
byteboss <adityasahrawat985@gmail.com>
2026-07-07 10:51:11 +0530

    feat: added vendor table and its fields

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express'
 
-const app = express();
+const app = express()
 
 app.get("/health", (req, res) => {
     return res.status(200).json({ message: "Server is running !" })
